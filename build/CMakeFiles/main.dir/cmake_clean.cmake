94c84c7d96d2425fa08c7e2c5880976acf793eb8
ppeix <2274029404@qq.com>
2017-04-02 12:13:25 +0800

    first commit

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/src/utils.cpp.o"
+  "CMakeFiles/main.dir/src/particles.cpp.o"
+  "CMakeFiles/main.dir/src/observation.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ readme.txt
@@ -0,0 +1,6 @@
+来自http://web.engr.oregonstate.edu/~hess/
+开始的时候用鼠标拖动出一个矩形框，为初始的跟踪目标，敲回车
+
+main.cpp里有main函数，可以选择captureFromCAM还是captureFromVideoFile
+
+GSL是一个开源的，用C／C++写成的数学函数库，非常流行也非常实用。需要安装gsl：The GNU Scientific Library 
